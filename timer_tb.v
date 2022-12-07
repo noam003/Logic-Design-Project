34ee2e29a0bdaf3f1545bd0fef163e41643c214b
lraiff <89608971+lraiff@users.noreply.github.com>
2022-12-07 16:26:57 -0500

    Update timer_tb so account for variable change

--- timer_tb.v
+++ timer_tb.v
@@ -1,23 +1,22 @@
-`timescale 1ns / 1ps
+`timescale 1 ns / 1ps
 
-// Last Modified by Laura 12/1/2021
+// Last Modified by Laura 12/7/2021
 module timer_tb();
 
     reg clk, reset, toggle;
-    wire [23:0] disp_time;
+    wire [26:0] out_time;
     
-    timer T0 (.clk(clk), .reset(reset), .toggle(toggle), .disp_time(disp_time));
+    timer T0 (.clk(clk), .reset(reset), .toggle(toggle), .out_time(out_time));
     
     initial begin
         clk = 0;
         reset = 1;
         toggle = 1;
+        #10000 $stop;
     end
     
     always #10 reset = 0;
     always #0.001 clk = ~clk;
-    always #500 toggle = 0;
     always #900 reset = 1;
     
-    always #3000 $stop;
 endmodule
