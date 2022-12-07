5fcc7c359e1b399278b3c95b91a923d03b09a9dc
lraiff <89608971+lraiff@users.noreply.github.com>
2022-12-07 16:26:09 -0500

    Update timer so it doesn't loop back after hitting 0 - Laura

--- timer.v
+++ timer.v
@@ -1,12 +1,13 @@
-`timescale 1ns / 1ps
+`timescale 1 ns / 1ps
 
-// Last Modified 12/1/2022 by Laura
-// timer starts at 23:59:59:99
+// Last Modified 12/7/2022 by Laura
+// timer starts at 01:59:59:99
 // count down timer
-module timer(toggle, reset, clk, disp_time);
+module timer(toggle, reset, clk, out_time);
     
     input toggle, reset, clk;
-    output reg [26:0] disp_time;
+    output reg [26:0] out_time;
+    reg [26:0] disp_time;
     
     reg [4:0] hr = 0;
     reg [5:0] min = 0;
@@ -16,7 +17,7 @@ module timer(toggle, reset, clk, disp_time);
     always @ (posedge reset) begin
         // reset timer to starting point
         if (reset) begin
-            hr <= 5'b10111;
+            hr <= 5'b00001;
             min <= 6'b111011;
             sec <= 6'b111011;
             ms <= 10'b1111101000;
@@ -25,26 +26,30 @@ module timer(toggle, reset, clk, disp_time);
     
     always @ (posedge clk) begin
         // while the timer is not set to stop, keep counting
-        if (toggle) begin
+        if (toggle && (disp_time != 27'b000010000010000010000000001)) begin
             ms <= ms - 1'b1;
-            end
+            out_time <= disp_time;
+        end
+        else if (disp_time == 27'b000010000010000010000000001) begin
+            out_time <= 0;
+        end
     end
     
     always @ (*) begin
         // if we count 1000ms, decrement the second
-        if (ms == 0) begin
+        if ((ms == 0) && (out_time != 0)) begin
             ms <= 10'b1111101000;
             sec <= sec - 1'b1;
         end
-        else if (sec == 0) begin
+        else if ((sec == 0) &&(out_time != 0)) begin
             sec <= 6'b111011;
             min <= min - 1'b1;
         end
-        else if (min == 0) begin
+        else if ((min == 0) &&(out_time != 0)) begin
             min <= 6'b111011;
             hr <= hr - 1'b1;
         end
-        else if (hr == 0) begin 
+        else if ((hr == 0)) begin 
             // stop the timer
             ms <= 0;
             sec <= 0;
@@ -53,7 +58,6 @@ module timer(toggle, reset, clk, disp_time);
         end
         
         disp_time <= {hr, min, sec, ms};
-        
     end
    
 
