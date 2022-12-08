7eb30d4a0c93ffb4114b31baa31f31bc8d40205a
lraiff <89608971+lraiff@users.noreply.github.com>
2022-12-08 16:16:31 -0500

    Update user_entry.v

--- user_entry.v
+++ user_entry.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 
-module user_entry(ms_sw, s_sw, min_sw, hr_sw, add_time, ms_o, sec_o, min_o, hr_o);
-    input ms_sw, s_sw, min_sw, hr_sw; // input switches
+module user_entry(ms_sw, s_sw, min_sw, hr_sw, add_time, toggle, ms_o, sec_o, min_o, hr_o);
+    input ms_sw, s_sw, min_sw, hr_sw, toggle; // input switches
     input add_time; // input button
     output reg [9:0] ms_o;
     output reg [5:0] sec_o;
@@ -9,13 +9,13 @@ module user_entry(ms_sw, s_sw, min_sw, hr_sw, add_time, ms_o, sec_o, min_o, hr_o
     output reg [4:0] hr_o;
     
     always @ (*) begin
-        if (ms_sw) begin
+        if (ms_sw && !toggle) begin
             ms_o <= ms_o + add_time;
-        end else if (s_sw) begin
+        end else if (s_sw && !toggle) begin
             sec_o <= sec_o + add_time;
-        end else if (min_sw) begin
+        end else if (min_sw && !toggle) begin
             min_o <= min_o + add_time;
-        end else if (hr_sw) begin
+        end else if (hr_sw && !toggle) begin
             hr_o <= hr_o + add_time;
         end
     end
