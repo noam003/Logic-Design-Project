c32026f5c3b1074b6570cb63d29a199255a4ba3b
noam003 <108095939+noam003@users.noreply.github.com>
2022-12-09 14:18:15 -0500

    Update timer.v

--- timer.v
+++ timer.v
@@ -16,52 +16,48 @@ module timer(toggle, ms_i, sec_i, min_i, hr_i, reset, clk, out_time);
     reg [5:0] min = 0;
     reg [5:0] sec = 0;
     reg [9:0] ms = 0;
+
     
-    always @ (posedge reset) begin
-        // reset timer to starting point
+    always @ (posedge clk or posedge reset) begin
         if (reset) begin
             hr <= hr_i;
             min <= min_i;
             sec <= sec_i;
             ms <= ms_i;
         end
-    end
-    
-    always @ (posedge clk) begin
         // while the timer is not set to stop, keep counting
-        if (toggle && (disp_time != 27'b000010000010000010000000001)) begin
-            ms <= ms - 1'b1;
-            out_time <= disp_time;
-        end
-        else if (disp_time == 27'b000010000010000010000000001) begin
-            out_time <= 0;
+       // if we count 1000ms, decrement the second
+        else begin
+            if ((ms == 0) && (out_time != 0)) begin
+                ms <= 10'b1111101000;
+                sec <= sec - 1'b1;
+            end
+            else if ((sec == 0) &&(out_time != 0)) begin
+                sec <= 6'b111011;
+                min <= min - 1'b1;
+            end
+            else if ((min == 0) &&(out_time != 0)) begin
+                min <= 6'b111011;
+                hr <= hr - 1'b1;
+            end
+            else if ((hr == 0)) begin 
+                // stop the timer
+                ms <= 0;
+                sec <= 0;
+                min <= 0;
+                hr <= 0;
+            end
+            
+            if (toggle && (disp_time != 27'b000010000010000010000000001)) begin
+                ms <= ms - 1'b1;
+                out_time <= disp_time;
+            end
+            else if (disp_time == 27'b000010000010000010000000001) begin
+                out_time <= 0;
+            end
+            
+            disp_time <= {hr, min, sec, ms};
         end
     end
     
-    always @ (*) begin
-        // if we count 1000ms, decrement the second
-        if ((ms == 0) && (out_time != 0)) begin
-            ms <= 10'b1111101000;
-            sec <= sec - 1'b1;
-        end
-        else if ((sec == 0) &&(out_time != 0)) begin
-            sec <= 6'b111011;
-            min <= min - 1'b1;
-        end
-        else if ((min == 0) &&(out_time != 0)) begin
-            min <= 6'b111011;
-            hr <= hr - 1'b1;
-        end
-        else if ((hr == 0)) begin 
-            // stop the timer
-            ms <= 0;
-            sec <= 0;
-            min <= 0;
-            hr <= 0;
-        end
-        
-        disp_time <= {hr, min, sec, ms};
-    end
-   
-
 endmodule
