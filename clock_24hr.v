d635e7cbd61ffea03c1de98c1ae4fa05a5ae202c
noam003 <108095939+noam003@users.noreply.github.com>
2022-12-09 11:01:27 -0500

    Update clock_24hr.v

--- clock_24hr.v
+++ clock_24hr.v
@@ -3,17 +3,30 @@
 // Module Name: clock_24hr
 // Description: Behavioral verilog Digita Ckock that increments every millisecond
 // Additional Comments: 1 khz = 1 ms, therefore we input the kh_clk; using active high rst
+// spring_season to enable day light adding, off for day light savings
 
+module clock_24hr(kh_clk, spring_szn, reset, disp_time);
 
-module clock_24hr(kh_clk, reset, disp_time);
-
-    input kh_clk, reset;
-    output reg [23:0] disp_time;
+    input kh_clk, spring_szn, reset;
+    output reg [26:0] disp_time;
+    reg [4:0] hr;
+    reg [5:0] min;
+    reg [5:0] sec;
+    reg [9:0] ms;
     
-    reg [4:0] hr = 0;
-    reg [5:0] min = 0;
-    reg [5:0] sec = 0;
-    reg [9:0] ms = 0;
+    initial begin
+        hr = 0;
+        min = 0;
+        sec = 0;
+        ms = 0;
+    end
+    
+   always @ (spring_szn) begin
+        if (spring_szn)
+            hr = hr + 1;
+        else 
+            hr = hr - 1;
+   end
     
     always @ (posedge kh_clk or posedge reset) begin
         if (reset) begin
@@ -38,7 +51,8 @@ module clock_24hr(kh_clk, reset, disp_time);
                     end
                 end
            end
-       end
+        end
        disp_time <= {hr,min,sec,ms};   
     end
+
 endmodule
