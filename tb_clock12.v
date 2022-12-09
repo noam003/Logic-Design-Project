be722773d875f912b595cfe4359e82375fd52e5f
noam003 <108095939+noam003@users.noreply.github.com>
2022-12-09 11:05:42 -0500

    Update tb_clock12.v

--- tb_clock12.v
+++ tb_clock12.v
@@ -8,13 +8,15 @@
 module tb_clock12( );
 
     reg clk;
+    reg spring_szn;
     reg reset;
-    wire [23:0] disp_time;
+    wire [26:0] disp_time;
     
-    clock_12hr digital_clock12(.kh_clk(clk), .reset(reset), .disp_time(disp_time));
+    clock_12hr digital_clock12(.kh_clk(clk), .spring_szn(spring_szn), .reset(reset), .disp_time(disp_time));
     
     initial begin 
         clk = 0;
+        spring_szn = 0;
     end
 
     always #1 clk = ~clk;
@@ -22,5 +24,7 @@ module tb_clock12( );
     initial begin
         reset = 1;
         #3 reset = 0;
+        #5000 spring_szn = ~spring_szn;
+        #6000 spring_szn = ~spring_szn;
     end
 endmodule
