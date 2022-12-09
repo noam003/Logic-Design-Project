f56993d2b3117b3ba0135696eee85945e225d398
noam003 <108095939+noam003@users.noreply.github.com>
2022-12-09 14:24:12 -0500

    Update top.v

--- top.v
+++ top.v
@@ -20,32 +20,29 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module top(clk, reset, toggle, select, ms_sw, sec_sw, min_sw, hr_sw, VGA_HS, VGA_VS, VGA_R, VGA_G, VGA_B);
-    input clk, reset, toggle;
-    input [1:0] select;
+module top(clk, reset, toggle, add_one, add_ten, select_nonDB, spring_szn, ms_sw, sec_sw, min_sw, hr_sw, VGA_HS, VGA_VS, VGA_R, VGA_G, VGA_B);
+    input clk, reset, spring_szn, toggle, add_one, add_ten, select_nonDB;
     input ms_sw, sec_sw, min_sw, hr_sw;
     output wire VGA_HS, VGA_VS;
     output wire [3:0] VGA_R, VGA_G, VGA_B;
-    reg [26:0] time_12hr, time_24hr, timer_out, stopwatch_out, mux_out;
-    reg [7:0] hr_bcd, min_bcd, sec_bcd, ms_bcd;
-    reg khz_clk;
-
+    wire [26:0] time_12hr, time_24hr, timer_out, stopwatch_out, mux_out;
+    wire [7:0] hr_bcd, min_bcd, sec_bcd, ms_bcd;
+    wire khz_clk;
+    wire [1:0] select;
     
-    clock_divider_khz(.clk_i(clk), .clk_o(khz_clk));
+    debouncer_select selectDB(.clock(clk), .reset(reset), .button_in(select_nonDB), .button_out(select_in));
     
-    clock_12hr clk12(.kh_clk(khz_clk), .reset(reset), .disp_time(time_12hr));
-    clock_24hr clk24(.kh_clk(khz_clk), .reset(reset), .disp_time(time_24hr));
-    timer_top t0(.toggle(toggle), .reset(reset), .clk(khz_clk), .ms_sw(ms_sw), .sec_sw(sec_sw), .min_sw(min_sw), .hr_sw(hr_sw), .out_time(timer_out));
+    counter get_select(.select_in(select_in), .reset(reset), .count(select));
+    
+    clock_divider_khz khzClk(.clk_i(clk), .clk_o(khz_clk));
+    
+    clock_12hr clk12(.kh_clk(khz_clk), .spring_szn(spring_szn), .reset(reset), .disp_time(time_12hr));
+    clock_24hr clk24(.kh_clk(khz_clk), .spring_szn(spring_szn), .reset(reset), .disp_time(time_24hr));
+    timer_top t0(.toggle(toggle), .reset(reset), .clk(khz_clk), .add_one(add_one), .add_ten(add_ten), .ms_sw(ms_sw), .s_sw(sec_sw), .min_sw(min_sw), .hr_sw(hr_sw), .out_time(timer_out));
     stopwatch s0(.clk(khz_clk), .reset(reset), .toggle(toggle), .disp_time(stopwatch_out));
-    
-    always @ (*) begin
-        case (select)
-            2'b00: mux_out <= time_12hr;
-            2'b01: mux_out <= time_24hr;
-            2'b10: mux_out <= timer_out;
-            2'b11: mux_out <= stopwatch_out;
-        endcase
-    end //always     
+  
+    // assign select = 2'b00;
+    mux_4to1 mux(.in1(time_12hr), .in2(time_24hr), .in3(timer_out), .in4(stopwatch_out), .select(select), .mux_out(mux_out));
         
     binary_to_bcd msBCD(.bin(mux_out[9:4]), .bcd(ms_bcd));
     binary_to_bcd secBCD(.bin(mux_out[15:10]), .bcd(sec_bcd));
