c039438b5a4247693b44a357de44e2a1aa7f9a85
noam003 <108095939+noam003@users.noreply.github.com>
2022-12-09 14:17:58 -0500

    Update stopwatch.v

--- stopwatch.v
+++ stopwatch.v
@@ -13,47 +13,44 @@ module stopwatch(clk, reset, toggle, disp_time);
     reg [9:0] ms = 0;
     
     //asynchronous reset
-    always @ (posedge reset) begin 
+    always @ (posedge clk or posedge reset) begin
         if (reset) begin
             ms <= 0;
             s <= 0;
             m <= 0;
             h <= 0;
         end
-    end
-    
-    always @ (posedge clk or posedge reset) begin
-        if (toggle) begin
-                ms <= ms +1'b1;
-        end
-    end
-    
-    //accounts for fall over bits, i.e. when s = 60, add 1 to m
-    always @ (*) begin
-        if (ms == 10'b1111101000) begin
-            ms <= 0;
-            s <= s + 1'b1;
-        end
-        else if (s == 6'b111100) begin
-            ms <= 0;
-            s <= 0;
-            m <= m + 1'b1;
-        end
-        else if (m == 6'b111100) begin
-            ms <= 0;
-            s <= 0;
-            m <= 0;
-            h <= h + 1'b1;
-        end
-        else if (h == 5'b11000) begin       //reset everything, max value
-            ms <= 0;
-            s <= 0;
-            m <= 0;
-            h <= 0;
-        end
-        
-        disp_time <= {h,m,s,ms};
+        //accounts for fall over bits, i.e. when s = 60, add 1 to m
+        else begin
+            if (ms == 10'b1111101000) begin
+                ms <= 0;
+                s <= s + 1'b1;
+            end
+            else if (s == 6'b111100) begin
+                ms <= 0;
+                s <= 0;
+                m <= m + 1'b1;
+            end
+            else if (m == 6'b111100) begin
+                ms <= 0;
+                s <= 0;
+                m <= 0;
+                h <= h + 1'b1;
+            end
+            else if (h == 5'b11000) begin       //reset everything, max value
+                ms <= 0;
+                s <= 0;
+                m <= 0;
+                h <= 0;
+            end
             
+            else if (toggle) begin
+                    ms <= ms +1'b1;
+            end
+            
+            disp_time <= {h,m,s,ms};
+        end
+
     end
     
     
