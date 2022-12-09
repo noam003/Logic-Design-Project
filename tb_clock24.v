e8ce088d7f8d22ee84e51371ebb251e1981e0c5a
noam003 <108095939+noam003@users.noreply.github.com>
2022-12-09 11:02:43 -0500

    Update tb_clock24.v

--- tb_clock24.v
+++ tb_clock24.v
@@ -5,13 +5,15 @@
 
 module tb_clock24();
     reg clk;
+    reg spring_szn;
     reg reset;
     wire [23:0] disp_time;
     
-    clock_24hr digital_clock24(.kh_clk(clk), .reset(reset), .disp_time(disp_time));
+    clock_24hr digital_clock24(.kh_clk(clk), .spring_szn(spring_szn), .reset(reset), .disp_time(disp_time));
     
     initial begin 
         clk = 0;
+        spring_szn = 0;
     end
 
     always #1 clk = ~clk;
@@ -19,5 +21,7 @@ module tb_clock24();
     initial begin
         reset = 1;
         #3 reset = 0;
+        #5000 spring_szn = ~spring_szn;
+        #6000 spring_szn = ~spring_szn;
     end
 endmodule
