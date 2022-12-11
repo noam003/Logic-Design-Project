e075d979f4df78478157dfe8844697c571322b12
lraiff <89608971+lraiff@users.noreply.github.com>
2022-12-11 13:18:36 -0500

    Update debouncer.v

--- debouncer.v
+++ debouncer.v
@@ -1,40 +1,31 @@
 module debouncer(
     input clk,
-    input resetn,
-    input button_in_one,
-    input button_in_ten,
-    input toggle,
-    output reg button_out,
-    output reg [1:0] button_type
+    input reset,
+    input button_in,
+    output reg button_out
     );
     
     reg output_exist;
     reg [19:0] deb_count;
     
-    parameter MAX = 100;
+    parameter MAX = 10;
     reg max_count_flag;
     
     always @ (posedge clk) begin
-        if (!resetn) begin
+        if (reset) begin
             deb_count <= 0;
             output_exist <= 0; 
             button_out <= 0;
+            max_count_flag <= 0;
         end else begin 
-            if (!toggle && (button_in_one || button_in_ten)) begin
+            if (button_in) begin
                 deb_count <= deb_count + 1;
-                
-                
-             
+
                 if ((deb_count == MAX) && (max_count_flag == 0)) begin
                     button_out <= 1'b1;
-                    deb_count <= deb_count; 
+                    deb_count <= 0; 
                     output_exist <= 1;
-                    max_count_flag <= 1;
-                    
-                    if (button_in_one)
-                        button_type <= 2'b01;
-                    if (button_in_ten)
-                        button_type <= 2'b10;
+                    max_count_flag <= 0;   
                 end
                 
                 if (output_exist) begin
@@ -42,7 +33,7 @@ module debouncer(
                     output_exist <= 0;
                 end 
             
-            end else if (!button_in_one || !button_in_ten) begin
+            end else if (!button_in) begin
                 deb_count <= 0;
                 button_out <= 0;
                 output_exist <= 0; 
