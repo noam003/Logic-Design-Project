a4622e90ccf10c0af254bd56e16adf7ae0a8d645
lraiff <89608971+lraiff@users.noreply.github.com>
2022-12-11 13:19:02 -0500

    Update count_out.v

--- count_out.v
+++ count_out.v
@@ -1,13 +1,27 @@
 `timescale 1ns / 1ps
 
-module count_out(button_in, out);
-    input [1:0] button_in;
+module count_out(button_in, button_type, clk, ms_sw, s_sw, min_sw, hr_sw, reset, out);
+    input reset, clk, ms_sw, s_sw, min_sw, hr_sw, button_in;
+    input [1:0] button_type;
     output reg [26:0] out;
     
-    always @ (*) begin
-        if (button_in == 2'b01)
-            out <= out + 1'b1;
-        else if (button_in == 2'b10)
-            out <= out + 4'b1010;
+    always @ (posedge clk or posedge reset) begin
+        if (reset)
+            out = 0;
+        else if (button_type == 2'b01 && button_in)
+            out = out + 1'b1;
+        else if (button_type == 2'b10 && button_in)
+            out = out + 4'b1010;
+        else
+            out = 0;
+            
+        if (ms_sw && out > 10'b1111101000)
+            out = out - 10'b1111101000;
+        else if ((s_sw || min_sw || hr_sw) && out > 6'b111011)
+            out = out - 6'b111011;
+    end
+    
+    always @ (posedge ms_sw or posedge s_sw or posedge min_sw or posedge hr_sw) begin
+        out = 0;
     end
 endmodule
