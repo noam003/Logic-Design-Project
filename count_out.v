f4640fc52284b81653c124e41d9088750291e8f0
lraiff <89608971+lraiff@users.noreply.github.com>
2022-12-11 17:07:16 -0500

    Update count_out.v

--- count_out.v
+++ count_out.v
@@ -1,27 +1,27 @@
 `timescale 1ns / 1ps
 
-module count_out(button_in, button_type, clk, ms_sw, s_sw, min_sw, hr_sw, reset, out);
-    input reset, clk, ms_sw, s_sw, min_sw, hr_sw, button_in;
+module count_out(button_in, button_type, count_in, clk, ms_sw, s_sw, min_sw, hr_sw, reset, out);
+    input reset, clk, ms_sw, s_sw, min_sw, hr_sw, button_in, count_in;
     input [1:0] button_type;
-    output reg [26:0] out;
-    
-    always @ (posedge clk or posedge reset) begin
-        if (reset)
+    output reg [26:0] out;   
+                                   
+    always @ (posedge clk) begin
+
+        if (reset || count_in == 0)
             out = 0;
-        else if (button_type == 2'b01 && button_in)
+        else if (button_type == 2'b01 && button_in) begin
             out = out + 1'b1;
-        else if (button_type == 2'b10 && button_in)
+        end else if (button_type == 2'b10 && button_in) begin
             out = out + 4'b1010;
-        else
+        end else
             out = 0;
             
         if (ms_sw && out > 10'b1111101000)
             out = out - 10'b1111101000;
         else if ((s_sw || min_sw || hr_sw) && out > 6'b111011)
             out = out - 6'b111011;
+            
     end
     
-    always @ (posedge ms_sw or posedge s_sw or posedge min_sw or posedge hr_sw) begin
-        out = 0;
-    end
+    
 endmodule
